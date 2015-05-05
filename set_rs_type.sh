bada08364bfd19bec9981a564191c16b21907fbf
BernieChen <s99056020@cs.nchu.edu.tw>
2015-05-05 16:10:15 +0000

    Modify gpio for RS422

--- set_rs_type.sh
+++ set_rs_type.sh
@@ -13,6 +13,7 @@ fi
 
 if [ "$1" == "rs422" ]; then
 
+     echo "1" > /sys/class/gpio/gpio204/value
      echo "1" > /sys/class/gpio/gpio205/value
      echo "1" > /sys/class/gpio/gpio206/value
      echo "1" > /sys/class/gpio/gpio207/value
